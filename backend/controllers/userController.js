0f9ac01c91879dc9e24b87f134107b4231199274
justin-hill426 <jrhill426@gmail.com>
2022-03-13 23:11:59 -0600

    Added express middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,13 @@
 // @route /api/users
 // @access Public
 const registerUser = (req, res) => {
+  const {name, email, password} = req.body
+
+  // Validation
+  if(!name || !email || !password) {
+    return res.status(400).json({message: 'Please includ all fields'})
+  }
+
   res.send('Register Route')
 }
 
