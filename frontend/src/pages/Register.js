a3335e36ead02e105e1c536cf1bfe3f3a592eaaf
justin-hill426 <jrhill426@gmail.com>
2022-03-15 11:51:27 -0600

    Added Redux to the project

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { useState } from 'react'
 import { FaUser } from 'react-icons/fa'
 import { toast } from 'react-toastify'
+import {useSelector, useDispatch} from 'react-redux'
+import { register } from '../features/auth/authSlice'
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -12,6 +14,10 @@ const Register = () => {
   })
 
   const {name, email, password, password2} = formData
+
+  const dispatch = useDispatch()
+
+  const {user, isLoading, isSuccess, message} = useSelector(state => state.auth)
 
   const onChange = (e) => {
     setFormData((prevState) => ({
@@ -26,6 +32,14 @@ const Register = () => {
 
     if(password !== password2) {
       toast.error('Passwords do not match')
+    } else {
+      const userData = {
+        name,
+        email,
+        password
+      }
+
+      dispatch(register(userData))
     }
   }
 
